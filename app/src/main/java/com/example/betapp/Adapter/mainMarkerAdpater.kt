22457e15167e301fabceb30ced33a9a8cf1524ee
anujomer21 <anujomer111@gmail.com>
2024-03-11 22:09:10 +0530

    time change

--- app/src/main/java/com/example/betapp/Adapter/mainMarkerAdpater.kt
+++ app/src/main/java/com/example/betapp/Adapter/mainMarkerAdpater.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
+import android.icu.util.TimeZone
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
@@ -23,6 +24,11 @@ import androidx.annotation.RequiresApi
 import com.example.betapp.GameActivity.GameGrid
 import com.example.betapp.R
 import com.example.betapp.model.market
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.InetAddress
+import java.net.URL
+import java.util.Date
 import java.util.Locale
 
 
@@ -37,16 +43,22 @@ class MarketAdapter(private val context: Context, private val marketList: List<m
 
     override fun onBindViewHolder(holder: MarketViewHolder, position: Int) {
         val market = marketList[position]
-val result=market.marketTodayOpenNumber.split("-")
+        val result = market.marketTodayOpenNumber.split("-")
         // Set data to views
         holder.marketName.text = market.marketName
         holder.centermarketWinningNumber.text = "-${result.get(1)}-"
-        holder.leftmarketWinningNumber.text=result.get(0)
-        holder.rightmarketWinningNumber.text=result.get(2)
+        holder.leftmarketWinningNumber.text = result.get(0)
+        holder.rightmarketWinningNumber.text = result.get(2)
         holder.marketOpenTime.text = market.marketOpenTime
         holder.marketCloseTime.text = market.marketCloseTime
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            holder.bind(market, market.openSessionOpenTime, market.openSessionCloseTime,market.closeSessionOpenTime,market.closeSessionCloseTime)
+            holder.bind(
+                market,
+                market.openSessionOpenTime,
+                market.openSessionCloseTime,
+                market.closeSessionOpenTime,
+                market.closeSessionCloseTime
+            )
         }
 
     }
@@ -57,16 +69,20 @@ val result=market.marketTodayOpenNumber.split("-")
 
     class MarketViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val marketName: TextView = itemView.findViewById(R.id.market_name)
-        val centermarketWinningNumber: TextView = itemView.findViewById(R.id.center_market_winning_number)
-        val leftmarketWinningNumber: TextView = itemView.findViewById(R.id.left_market_winning_number)
-        val rightmarketWinningNumber: TextView = itemView.findViewById(R.id.right_market_winning_number)
+        val centermarketWinningNumber: TextView =
+            itemView.findViewById(R.id.center_market_winning_number)
+        val leftmarketWinningNumber: TextView =
+            itemView.findViewById(R.id.left_market_winning_number)
+        val rightmarketWinningNumber: TextView =
+            itemView.findViewById(R.id.right_market_winning_number)
         val marketBettingStatus: TextView = itemView.findViewById(R.id.market_betting_status_title)
         val marketOpenTime: TextView = itemView.findViewById(R.id.market_open_time)
         val marketCloseTime: TextView = itemView.findViewById(R.id.market_close_time)
-        val play:TextView=itemView.findViewById(R.id.play)
+        val play: TextView = itemView.findViewById(R.id.play)
 
         private val handler = Handler(Looper.getMainLooper())
         private val delay = 10000L // 1 second delay
+
         @RequiresApi(Build.VERSION_CODES.O)
         fun bind(
             item: market,
@@ -80,16 +96,29 @@ val result=market.marketTodayOpenNumber.split("-")
             // Schedule a periodic check to update the status
             handler.postDelayed(object : Runnable {
                 override fun run() {
-                    updateStatus(item, openTime, closeTime,closeSessionOpenTime,closeSessionCloseTime)
+                    updateStatus(
+                        item,
+                        openTime,
+                        closeTime,
+                        closeSessionOpenTime,
+                        closeSessionCloseTime
+                    )
                     handler.postDelayed(this, delay)
                 }
             }, delay)
             play.setOnClickListener {
-                handleButtonClick(item, openTime, closeTime,closeSessionOpenTime,closeSessionCloseTime)
+                handleButtonClick(
+                    item,
+                    openTime,
+                    closeTime,
+                    closeSessionOpenTime,
+                    closeSessionCloseTime
+                )
             }
 
 
         }
+
         @RequiresApi(Build.VERSION_CODES.O)
         private fun handleButtonClick(
             item: market,
@@ -99,34 +128,33 @@ val result=market.marketTodayOpenNumber.split("-")
             closeSessionCloseTime: String
         ) {
             val currentTime = getCurrentTime()
-            Log.d("time","${isTimeBetween(currentTime, openTime, closeTime)}")
-             if (isTimeBetween(currentTime, openTime, closeTime)) {
+            Log.d("time", "${isTimeBetween( openTime, closeTime)}")
+            if (isTimeBetween( openTime, closeTime)) {
 
                 val intent = Intent(itemView.context, GameGrid::class.java)
-                intent.putExtra("marketId",item.marketId)
-                intent.putExtra("markerName",item.marketName)
-                 intent.putExtra("start_time",openTime)
-                 intent.putExtra("close_time",closeTime)
-                intent.putExtra("session","open")
+                intent.putExtra("marketId", item.marketId)
+                intent.putExtra("markerName", item.marketName)
+                intent.putExtra("start_time", openTime)
+                intent.putExtra("close_time", closeTime)
+                intent.putExtra("session", "open")
                 itemView.context.startActivity(intent)
 
-            }
-            else if(isTimeBetween(currentTime,closeSessionOpenTime,closeSessionCloseTime)){
-                 val intent = Intent(itemView.context, GameGrid::class.java)
-                 intent.putExtra("marketId",item.marketId)
-                 intent.putExtra("markerName",item.marketName)
-                 intent.putExtra("session","close")
-                 intent.putExtra("start_time",closeSessionOpenTime)
-                 intent.putExtra("close_time",closeSessionCloseTime)
-                 itemView.context.startActivity(intent)
-             }
-            else {
+            } else if (isTimeBetween( closeSessionOpenTime, closeSessionCloseTime)) {
+                val intent = Intent(itemView.context, GameGrid::class.java)
+                intent.putExtra("marketId", item.marketId)
+                intent.putExtra("markerName", item.marketName)
+                intent.putExtra("session", "close")
+                intent.putExtra("start_time", closeSessionOpenTime)
+                intent.putExtra("close_time", closeSessionCloseTime)
+                itemView.context.startActivity(intent)
+            } else {
                 // Betting is closed, vibrate the phone and show a toast
                 vibratePhone()
                 shakeButton()
                 showToast("Market is closed for betting")
             }
         }
+
         private fun shakeButton() {
             val shakeAnimation = TranslateAnimation(0f, 10f, 0f, 0f)
             shakeAnimation.duration = 500 // Shake for 500 milliseconds
@@ -139,8 +167,14 @@ val result=market.marketTodayOpenNumber.split("-")
             val vibrator = itemView.context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
             if (vibrator.hasVibrator()) {
-                vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
-            }   }
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        500,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+            }
+        }
 
         private fun showToast(message: String) {
             Toast.makeText(itemView.context, message, Toast.LENGTH_SHORT).show()
@@ -156,21 +190,24 @@ val result=market.marketTodayOpenNumber.split("-")
             val currentTime = getCurrentTime()
 
             itemView.post {
-                if (isTimeBetween(currentTime, openTime, closeTime)) {
+                if (isTimeBetween( openTime, closeTime)) {
                     setStatus(item, "Betting open", R.color.green_color)
-                }else
-                    if(isTimeBetween(currentTime,closeSessionOpenTime,closeSessionCloseTime)){
+                } else
+                    if (isTimeBetween( closeSessionOpenTime, closeSessionCloseTime)) {
                         setStatus(item, "Betting open in Close Time", R.color.green_color)
+                    } else {
+                        setStatus(item, "Market closed", android.R.color.holo_red_dark)
                     }
-                else {
-                    setStatus(item, "Market closed", android.R.color.holo_red_dark)
-                }
             }
         }
 
         private fun setStatus(item: market, message: String, colorResId: Int) {
             marketBettingStatus.text = message
-            marketBettingStatus.setTextColor(marketBettingStatus.context.resources.getColor(colorResId))
+            marketBettingStatus.setTextColor(
+                marketBettingStatus.context.resources.getColor(
+                    colorResId
+                )
+            )
             // You may want to update other views in your item layout based on the status.
         }
 
@@ -179,21 +216,54 @@ val result=market.marketTodayOpenNumber.split("-")
             return dateFormat.format(Calendar.getInstance().time)
         }
 
-        private fun isTimeBetween(currentTime: String, openTime: String, closeTime: String): Boolean {
+        private fun isTimeBetween(
+            openTime: String,
+            closeTime: String
+        ): Boolean {
             try {
-                val parser = SimpleDateFormat("hh:mm a", Locale.getDefault())
-                val currentTimeDate = parser.parse(currentTime)
-                val openTimeDate = parser.parse(openTime)
-                val closeTimeDate = parser.parse(closeTime)
-                Log.d("time","$openTime , $closeTime ,$currentTime")
-                return currentTimeDate in openTimeDate..closeTimeDate
-            }
-            catch (e:Exception){
-                return false
-            }
+                // Get the current Indian time from the internet
+                val currentTimeIndian = getCurrentTimeFromInternet()
+                if (currentTimeIndian != null) {
+                    val parser = SimpleDateFormat("hh:mm a", Locale.getDefault())
+                    parser.timeZone = TimeZone.getTimeZone("Asia/Kolkata")
 
+                    // Parse the open and close times to Indian time zone
+                    val openTimeDate = parser.parse(openTime)
+                    val closeTimeDate = parser.parse(closeTime)
+
+                    // Check if the current time is between the open and close times
+                    return currentTimeIndian in openTimeDate..closeTimeDate
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return false
         }
-    }
+
+
+        fun getCurrentTimeFromInternet(): Date? {
+            val url = URL("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+            val connection = url.openConnection()
+            val inputStream = connection.getInputStream()
+            val reader = BufferedReader(InputStreamReader(inputStream))
+
+            val response = StringBuilder()
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                response.append(line)
+            }
+
+            reader.close()
+            inputStream.close()
+
+            val jsonResponse = response.toString()
+            val currentTime = jsonResponse.substringAfter("\"datetime\":\"").substringBefore("\"")
+
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            return dateFormat.parse(currentTime)
+        }
+
 
     }
+}
 
--- app/src/main/java/com/example/betapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/betapp/api/RetrofitInstance.kt
@@ -3,7 +3,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance{
-     val Base_url="https://works.diginspire.in/jannat/api/"
+     val Base_url="https://marutibets.com/api/"
     val instance:ApiService by lazy{
         val retrofit = Retrofit.Builder()
             .baseUrl(Base_url)
